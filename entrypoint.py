864ce3f56cd4d593f068421701ff38c125f5fe08
Burton Gus <carloscarvalho@hotmail.com>
2022-05-04 21:30:28 -0300

    feat: Creates Initial Code

---
+++ entrypoint.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S python3 -B
+
+import sys
+
+if __name__ == "__main__" :
+    input1 = sys.argv[1]
+    input2 = sys.argv[2]
+
+    print("::set-output name=output-one::" + output1)
+    print("::set-output name=output-two::" + output2)
+
