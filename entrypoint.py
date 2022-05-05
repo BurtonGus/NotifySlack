3ac376e06560a43915f3e64f427f95bcb2f971be
Burton Gus <carloscarvalho@hotmail.com>
2022-05-04 21:51:44 -0300

    fix: NameError: name 'output1' is not defined

--- entrypoint.py
+++ entrypoint.py
@@ -5,7 +5,8 @@ import sys
 if __name__ == "__main__" :
     input1 = sys.argv[1]
     input2 = sys.argv[2]
-
+    output1 = "1"
+    output2 = "2"
     print("::set-output name=output-one::" + output1)
     print("::set-output name=output-two::" + output2)
 
